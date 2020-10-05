19c93a66bd8753335a301e451892b1e230d358d0
Никита <world.of.tanks.96@inbox.ru>
2020-10-06 00:45:53 +0300

    Доработан модуль обнаружения новых файлов

--- MODULE__RESERVE_NEW_FILE_DETECTOR/Detector.cs
+++ MODULE__RESERVE_NEW_FILE_DETECTOR/Detector.cs
@@ -45,10 +45,11 @@ namespace MODULE__RESERVE_NEW_FILE_DETECTOR
 
     public static class PartitionMonitor
     {
-        private static Task[] PartitionMonitors = new Task[0];
         public static Thread CommandExecuter = new Thread(CommandThread);
+
         private static FileSystemWatcher[] FileSystemWatchers = new FileSystemWatcher[0];
-        
+        private static object locker = new object();
+
         private static NamedPipeClientStream ClientStream = new NamedPipeClientStream("PartitionMon_FilePaths");
         private static NamedPipeServerStream CommandStream = new NamedPipeServerStream("PartitionMon_Command");
 
@@ -59,7 +60,7 @@ namespace MODULE__RESERVE_NEW_FILE_DETECTOR
         {
 #if DEBUG
             Debugger.StartDebug();
-            Debugger.WriteLog(Debugger.LogLevel.Warning, $"[FileDetector] Wait connection to pipe PartitionMon_FilePaths"); 
+            Debugger.WriteLog(Debugger.LogLevel.Warning, $"[FileDetector] Wait connection to pipe PartitionMon_FilePaths");
 #endif
 
             ClientStream.Connect();
@@ -90,69 +91,113 @@ namespace MODULE__RESERVE_NEW_FILE_DETECTOR
                 if (buffer.Length > 0)
                 {
                     //Парсинг операндов
-                    string op1 = string.Empty, op2 = string.Empty;
-                    
+                    string op1 = string.Empty,
+                           op2 = string.Empty;
+
                     try
                     {
-                        op1 = buffer.Substring(buffer.IndexOf('*') + 1, buffer.Length - buffer.IndexOf('&') - 1);
+                        Console.WriteLine("Try parse op1 ");
+                        op1 = buffer.Substring(buffer.IndexOf('*') + 1, buffer.IndexOf('&') - 2);
                         op2 = buffer.Substring(buffer.IndexOf('&') + 1);
                     }
                     catch
                     {
 #if DEBUG
-                        Debugger.WriteLog($"[FileDetector] [CommandThread] Error parse operands");
+                        Debugger.WriteLog($"[FileDetector] [CommandThread] Parameter parsing error(receive command ->{buffer}<-)");
 #endif
                         continue;
                     }
 #if DEBUG
-                    Debugger.WriteLog($"[FileDetector] [CommandThread] op1 and op2 = \"{op1}\" and \"{op2}\" ");
+                    Debugger.WriteLog($"[FileDetector] [CommandThread] ({buffer}) parsed =>>> op1 and op2 = \"{op1}\" and \"{op2}\" ");
 #endif
 
                     switch (buffer[0])
                     {
-                        case '0': 
+                        //command id 0 - Create Partition Monitor
+                        case '0':
                             {
-                                //Создание монитора раздела
+                                if (op2.Length <= 1) op2 = "*.*";
+#if DEBUG
+                                Debugger.WriteLog($"[FileDetector] Create Partition monitor for \"{op1}\", used filter \"{op2}\"");
+#endif
                                 CreatePartitionMon(op1, op2);
-                                break; 
-                            }
-
-                        case '1':
-                            {
-                                //Отключение монитора раздела
                                 break;
                             }
 
+                        //command id 1 - Disable partition monitor 
+                        case '1':
+                            {
+#if DEBUG
+                                Debugger.WriteLog($"[FileDetector] Create Partition monitor for \"{op1}\", used filter \"{op2}\"");
+#endif
+                                DisablePartitionMon(op1);
+                                break;
+                            }
 
+#if DEBUG
+                        default:
+                            {
+                                Debugger.WriteLog("[FileDetector] [CommandThread] Command not found");
+                                break;
+                            }
+#endif
                     }
                 }
 
             }
         }
 
-
-
+        /// <summary>
+        /// Create and enable FileSystemWatcher for partition
+        /// </summary>
+        /// <param name="PartitionPath"></param>
+        /// <param name="Filter"></param>
         public static void CreatePartitionMon(string PartitionPath, string Filter)
         {
+            lock (locker)
+            {
+                Array.Resize(ref FileSystemWatchers, FileSystemWatchers.Length + 1);
+                FileSystemWatchers[FileSystemWatchers.Length - 1] = new FileSystemWatcher(PartitionPath, Filter);
+
+                FileSystemWatchers[FileSystemWatchers.Length - 1].Created += CreateFileEvent;
+                FileSystemWatchers[FileSystemWatchers.Length - 1].Changed += ChangedFileEvent;
+                FileSystemWatchers[FileSystemWatchers.Length - 1].IncludeSubdirectories = true;
+                FileSystemWatchers[FileSystemWatchers.Length - 1].EnableRaisingEvents = true;
+            }
+        }
+
+        /// <summary>
+        /// Disable FileSystemWatcher for partition
+        /// </summary>
+        /// <param name="PartitionPath"></param>
+        public static void DisablePartitionMon(string PartitionPath)
+        {
 #if DEBUG
-            Debugger.WriteLog($"[FileDetector] Create Partition monitor for \"{PartitionPath}\", used filter \"{Filter}\"");
+            Debugger.WriteLog(Debugger.LogLevel.Warning, $"[FileDetector] [CommandThread] (DisablePartitionMon) Remove monitor for \"{PartitionPath}\"");
 #endif
-
-            FileSystemWatcher systemWatcher = new FileSystemWatcher(PartitionPath, Filter);
-
-            systemWatcher.Created += CreateFileEvent;
-            systemWatcher.Changed += ChangedFileEvent;
-            systemWatcher.EnableRaisingEvents = true;
-            
-            Array.Resize(ref FileSystemWatchers, FileSystemWatchers.Length + 1);
-
-            FileSystemWatchers[FileSystemWatchers.Length - 1] = systemWatcher;
+            lock (locker)
+            {
+                for (int index = 0; index < FileSystemWatchers.Length; index++)
+                {
+                    if (FileSystemWatchers[index].Path == PartitionPath)
+                    {
+                        {
+                            FileSystemWatchers[index].Dispose();
+                            FileSystemWatchers[index] = FileSystemWatchers[FileSystemWatchers.Length - 1];
+                            Array.Resize(ref FileSystemWatchers, FileSystemWatchers.Length - 1);
+#if DEBUG
+                            Debugger.WriteLog(Debugger.LogLevel.Info, $"[FileDetector] [CommandThread] (DisablePartitionMon) SUCCESS Remove monitor for \"{PartitionPath}\" ");
+#endif
+                        }
+                    }
+                }
+            }
         }
 
         static void CreateFileEvent(object sender, FileSystemEventArgs e)
         {
 #if DEBUG
-            Debugger.WriteLog($"[FileDetector] [CreateFileEvent] Detected create file {e.Name}");
+            Writer.WriteLine($"[FileDetector] [CreateFileEvent] Detected create file {e.Name}");
 #endif
             Writer.WriteLine((int)e.ChangeType + e.FullPath);
         }
@@ -164,6 +209,21 @@ namespace MODULE__RESERVE_NEW_FILE_DETECTOR
 #endif
             Writer.WriteLine((int)e.ChangeType + e.FullPath);
         }
+
+        /// <summary>
+        /// Disable all threads and FIleSys watchers
+        /// </summary>
+        public static void StopAll()
+        {
+            CommandExecuter.Abort();
+            ClientStream.Close();
+            CommandStream.Close();
+
+            foreach (FileSystemWatcher watcher in FileSystemWatchers)
+            {
+                watcher.Dispose();
+            }
+        }
     }
 
     public static class Initializator
@@ -172,6 +232,11 @@ namespace MODULE__RESERVE_NEW_FILE_DETECTOR
         {
             PartitionMonitor.CommandExecuter.Start();
             return 0;
+        }
+
+        public static void Stop()
+        {
+            PartitionMonitor.StopAll();
         }
     }
 }
